1674fdb3114c7b38eb9745dd595f6350048dd938
mataktelis11 <61196956+mataktelis11@users.noreply.github.com>
2023-01-20 15:33:41 +0200

    Added Professor and Secretary controller using Entity Framework

---
+++ UniversityApp/Controllers/ProfessorsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using UniversityApp.Models;
+
+namespace UniversityApp.Controllers
+{
+    public class ProfessorsController : Controller
+    {
+        private readonly UniversityDBContext _context;
+
+        public ProfessorsController(UniversityDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Professors
+        public async Task<IActionResult> Index()
+        {
+            var universityDBContext = _context.Professors.Include(p => p.User);
+            return View(await universityDBContext.ToListAsync());
+        }
+
+        // GET: Professors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Professors == null)
+            {
+                return NotFound();
+            }
+
+            var professor = await _context.Professors
+                .Include(p => p.User)
+                .FirstOrDefaultAsync(m => m.ProfessorId == id);
+            if (professor == null)
+            {
+                return NotFound();
+            }
+
+            return View(professor);
+        }
+
+        // GET: Professors/Create
+        public IActionResult Create()
+        {
+            ViewData["Userid"] = new SelectList(_context.Users, "Userid", "Userid");
+            return View();
+        }
+
+        // POST: Professors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProfessorId,Afm,Name,Surname,Department,Userid")] Professor professor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(professor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Userid"] = new SelectList(_context.Users, "Userid", "Userid", professor.Userid);
+            return View(professor);
+        }
+
+        // GET: Professors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Professors == null)
+            {
+                return NotFound();
+            }
+
+            var professor = await _context.Professors.FindAsync(id);
+            if (professor == null)
+            {
+                return NotFound();
+            }
+            ViewData["Userid"] = new SelectList(_context.Users, "Userid", "Userid", professor.Userid);
+            return View(professor);
+        }
+
+        // POST: Professors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProfessorId,Afm,Name,Surname,Department,Userid")] Professor professor)
+        {
+            if (id != professor.ProfessorId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(professor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProfessorExists(professor.ProfessorId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Userid"] = new SelectList(_context.Users, "Userid", "Userid", professor.Userid);
+            return View(professor);
+        }
+
+        // GET: Professors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Professors == null)
+            {
+                return NotFound();
+            }
+
+            var professor = await _context.Professors
+                .Include(p => p.User)
+                .FirstOrDefaultAsync(m => m.ProfessorId == id);
+            if (professor == null)
+            {
+                return NotFound();
+            }
+
+            return View(professor);
+        }
+
+        // POST: Professors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Professors == null)
+            {
+                return Problem("Entity set 'UniversityDBContext.Professors'  is null.");
+            }
+            var professor = await _context.Professors.FindAsync(id);
+            if (professor != null)
+            {
+                _context.Professors.Remove(professor);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProfessorExists(int id)
+        {
+          return _context.Professors.Any(e => e.ProfessorId == id);
+        }
+    }
+}
---
+++ UniversityApp/Controllers/SecretariesController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using UniversityApp.Models;
+
+namespace UniversityApp.Controllers
+{
+    public class SecretariesController : Controller
+    {
+        private readonly UniversityDBContext _context;
+
+        public SecretariesController(UniversityDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Secretaries
+        public async Task<IActionResult> Index()
+        {
+            var universityDBContext = _context.Secretaries.Include(s => s.User);
+            return View(await universityDBContext.ToListAsync());
+        }
+
+        // GET: Secretaries/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Secretaries == null)
+            {
+                return NotFound();
+            }
+
+            var secretary = await _context.Secretaries
+                .Include(s => s.User)
+                .FirstOrDefaultAsync(m => m.SecretaryId == id);
+            if (secretary == null)
+            {
+                return NotFound();
+            }
+
+            return View(secretary);
+        }
+
+        // GET: Secretaries/Create
+        public IActionResult Create()
+        {
+            ViewData["Userid"] = new SelectList(_context.Users, "Userid", "Userid");
+            return View();
+        }
+
+        // POST: Secretaries/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SecretaryId,Name,Surname,Department,Userid")] Secretary secretary)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(secretary);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Userid"] = new SelectList(_context.Users, "Userid", "Userid", secretary.Userid);
+            return View(secretary);
+        }
+
+        // GET: Secretaries/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Secretaries == null)
+            {
+                return NotFound();
+            }
+
+            var secretary = await _context.Secretaries.FindAsync(id);
+            if (secretary == null)
+            {
+                return NotFound();
+            }
+            ViewData["Userid"] = new SelectList(_context.Users, "Userid", "Userid", secretary.Userid);
+            return View(secretary);
+        }
+
+        // POST: Secretaries/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SecretaryId,Name,Surname,Department,Userid")] Secretary secretary)
+        {
+            if (id != secretary.SecretaryId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(secretary);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SecretaryExists(secretary.SecretaryId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Userid"] = new SelectList(_context.Users, "Userid", "Userid", secretary.Userid);
+            return View(secretary);
+        }
+
+        // GET: Secretaries/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Secretaries == null)
+            {
+                return NotFound();
+            }
+
+            var secretary = await _context.Secretaries
+                .Include(s => s.User)
+                .FirstOrDefaultAsync(m => m.SecretaryId == id);
+            if (secretary == null)
+            {
+                return NotFound();
+            }
+
+            return View(secretary);
+        }
+
+        // POST: Secretaries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Secretaries == null)
+            {
+                return Problem("Entity set 'UniversityDBContext.Secretaries'  is null.");
+            }
+            var secretary = await _context.Secretaries.FindAsync(id);
+            if (secretary != null)
+            {
+                _context.Secretaries.Remove(secretary);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SecretaryExists(int id)
+        {
+          return _context.Secretaries.Any(e => e.SecretaryId == id);
+        }
+    }
+}
