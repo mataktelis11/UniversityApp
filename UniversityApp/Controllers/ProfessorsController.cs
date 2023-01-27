03b2b79c5afbfd0769fefd9d56fe2f23a7093f54
mataktelis11 <61196956+mataktelis11@users.noreply.github.com>
2023-01-27 22:44:50 +0200

    Removed Professor useless methos and views #2

--- UniversityApp/Controllers/ProfessorsController.cs
+++ UniversityApp/Controllers/ProfessorsController.cs
@@ -64,6 +64,7 @@ namespace UniversityApp.Controllers
 
         // GET: Professors/RegisteredStudents/6
         // Retrieve Registered Students of a Course.
+        [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
         public async Task<IActionResult> RegisteredStudents(int? id)
         {
             if (HttpContext.Session.GetString("userid") == null)
@@ -94,6 +95,8 @@ namespace UniversityApp.Controllers
         [HttpPost]
         public async Task<IActionResult> UploadGrades(int CourseId, IFormFile usercsv)
         {
+            // add checks auth
+
             string filename = usercsv.FileName;
             filename = Path.GetFileName(filename);
 
@@ -148,6 +151,7 @@ namespace UniversityApp.Controllers
             return RedirectToAction("RegisteredStudents", new { id = CourseId });
         }
 
+        [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
         public async Task<IActionResult> EditGrades(int id)
         {
             if (HttpContext.Session.GetString("userid") == null)
@@ -202,141 +206,6 @@ namespace UniversityApp.Controllers
             var professor = await _context.Professors.Where(p => p.Userid.ToString().Equals(userid)).FirstOrDefaultAsync();*/
 
             return RedirectToAction("RegisteredStudents", new { id = id });
-        }
-
-
-        // GET: Professors/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Professors == null)
-            {
-                return NotFound();
-            }
-
-            var professor = await _context.Professors
-                .Include(p => p.User)
-                .FirstOrDefaultAsync(m => m.ProfessorId == id);
-            if (professor == null)
-            {
-                return NotFound();
-            }
-
-            return View(professor);
-        }
-
-        // GET: Professors/Create
-        public IActionResult Create()
-        {
-            ViewData["Userid"] = new SelectList(_context.Users, "Userid", "Userid");
-            return View();
-        }
-
-        // POST: Professors/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ProfessorId,Afm,Name,Surname,Department,Userid")] Professor professor)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(professor);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["Userid"] = new SelectList(_context.Users, "Userid", "Userid", professor.Userid);
-            return View(professor);
-        }
-
-        // GET: Professors/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Professors == null)
-            {
-                return NotFound();
-            }
-
-            var professor = await _context.Professors.FindAsync(id);
-            if (professor == null)
-            {
-                return NotFound();
-            }
-            ViewData["Userid"] = new SelectList(_context.Users, "Userid", "Userid", professor.Userid);
-            return View(professor);
-        }
-
-        // POST: Professors/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ProfessorId,Afm,Name,Surname,Department,Userid")] Professor professor)
-        {
-            if (id != professor.ProfessorId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(professor);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProfessorExists(professor.ProfessorId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["Userid"] = new SelectList(_context.Users, "Userid", "Userid", professor.Userid);
-            return View(professor);
-        }
-
-        // GET: Professors/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Professors == null)
-            {
-                return NotFound();
-            }
-
-            var professor = await _context.Professors
-                .Include(p => p.User)
-                .FirstOrDefaultAsync(m => m.ProfessorId == id);
-            if (professor == null)
-            {
-                return NotFound();
-            }
-
-            return View(professor);
-        }
-
-        // POST: Professors/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Professors == null)
-            {
-                return Problem("Entity set 'UniversityDBContext.Professors'  is null.");
-            }
-            var professor = await _context.Professors.FindAsync(id);
-            if (professor != null)
-            {
-                _context.Professors.Remove(professor);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool ProfessorExists(int id)
