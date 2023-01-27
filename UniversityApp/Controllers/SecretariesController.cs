47ab07e4ece7b94f645e32bde469eb86eef90e46
mataktelis11 <61196956+mataktelis11@users.noreply.github.com>
2023-01-27 23:00:40 +0200

    Removed useless methods and views of Secretaries #5

--- UniversityApp/Controllers/SecretariesController.cs
+++ UniversityApp/Controllers/SecretariesController.cs
@@ -28,6 +28,7 @@ namespace UniversityApp.Controllers
 
         // GET: Secretaries/Account
         // Secretay's wellcome page
+        [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
         public async Task<IActionResult> Account()
         {
             if (HttpContext.Session.GetString("userid") == null)
@@ -44,6 +45,7 @@ namespace UniversityApp.Controllers
 
         // GET: Secretaries/UniversityCourses
         // Retrieve all the courses
+        [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
         public async Task<IActionResult> UniversityCourses()
         {
             if (HttpContext.Session.GetString("userid") == null)
@@ -58,6 +60,7 @@ namespace UniversityApp.Controllers
 
         // GET: Secretaries/CreateCourse
         // Form to create a new Course
+        [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
         public IActionResult CreateCourse()
         {
             List<SelectListItem> professors = new SelectList(_context.Professors, "ProfessorId", "ProfessorId").ToList();
@@ -103,6 +106,7 @@ namespace UniversityApp.Controllers
 
         // GET: Secretaries/UniversityCourses/6
         // Form to assign a professor for a given Course
+        [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
         public async Task<IActionResult> AssignProfessor(int id)
         {
             if (HttpContext.Session.GetString("userid") == null)
@@ -161,15 +165,29 @@ namespace UniversityApp.Controllers
 
         // GET: Secretaries/UniversityStudents
         // obtain all the students
+        [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
         public async Task<IActionResult> UniversityStudents()
         {
+            if (HttpContext.Session.GetString("userid") == null)
+                return View("AuthorizationError");
+
+            if (!(HttpContext.Session.GetString("role").Equals("Secretaries")))
+                return View("NoRightsError");
+
             var UniversityDBContext = _context.Students;
             return View(await UniversityDBContext.ToListAsync());
         }
 
         // GET: Secretaries/CreateStudent
+        [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
         public IActionResult CreateStudent()
-        {           
+        {
+            if (HttpContext.Session.GetString("userid") == null)
+                return View("AuthorizationError");
+
+            if (!(HttpContext.Session.GetString("role").Equals("Secretaries")))
+                return View("NoRightsError");
+
             return View();
         }
 
@@ -216,8 +234,15 @@ namespace UniversityApp.Controllers
 
 
         // GET: Secretaries/StudentDetails/6
+        [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
         public async Task<IActionResult> StudentDetails(int? id)
         {
+            if (HttpContext.Session.GetString("userid") == null)
+                return View("AuthorizationError");
+
+            if (!(HttpContext.Session.GetString("role").Equals("Secretaries")))
+                return View("NoRightsError");
+
             if (id == null || _context.Students == null)
             {
                 return NotFound();
@@ -239,8 +264,15 @@ namespace UniversityApp.Controllers
 
 
         // GET: Secretaries/StudentAssignCourses/6
+        [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
         public async Task<IActionResult> StudentAssignCourses(int? id)
         {
+            if (HttpContext.Session.GetString("userid") == null)
+                return View("AuthorizationError");
+
+            if (!(HttpContext.Session.GetString("role").Equals("Secretaries")))
+                return View("NoRightsError");
+
             if (id == null || _context.Students == null)
             {
                 return NotFound();
@@ -328,13 +360,21 @@ namespace UniversityApp.Controllers
 
         // GET: Secretaries/UniversityProfessors
         // obtain all the Professors
+        [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
         public async Task<IActionResult> UniversityProfessors()
         {
+            if (HttpContext.Session.GetString("userid") == null)
+                return View("AuthorizationError");
+
+            if (!(HttpContext.Session.GetString("role").Equals("Secretaries")))
+                return View("NoRightsError");
+
             var UniversityDBContext = _context.Professors;
             return View(await UniversityDBContext.ToListAsync());
         }
 
         // GET: Secretaries/CreateProfessor
+        [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
         public IActionResult CreateProfessor()
         {
             return View();
@@ -378,8 +418,16 @@ namespace UniversityApp.Controllers
 
 
         // GET: Secretaries/ProfessorDetails/6
+        [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
         public async Task<IActionResult> ProfessorDetails(int? id)
         {
+            if (HttpContext.Session.GetString("userid") == null)
+                return View("AuthorizationError");
+
+            if (!(HttpContext.Session.GetString("role").Equals("Secretaries")))
+                return View("NoRightsError");
+
+
             if (id == null || _context.Professors == null)
             {
                 return NotFound();
@@ -421,152 +469,6 @@ namespace UniversityApp.Controllers
             await _context.SaveChangesAsync();
 
             return RedirectToAction("ProfessorDetails", new { id = professorid });
-        }
-
-
-        //// GET: Professors/Create
-        //public IActionResult Create()
-        //{
-        //    ViewData["Userid"] = new SelectList(_context.Users, "Userid", "Userid");
-        //    return View();
-        //}
-
-
-        // bellow are the build in methods
-
-
-        // GET: Secretaries/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Secretaries == null)
-            {
-                return NotFound();
-            }
-
-            var secretary = await _context.Secretaries
-                .Include(s => s.User)
-                .FirstOrDefaultAsync(m => m.SecretaryId == id);
-            if (secretary == null)
-            {
-                return NotFound();
-            }
-
-            return View(secretary);
-        }
-
-        // GET: Secretaries/Create
-        public IActionResult Create()
-        {
-            ViewData["Userid"] = new SelectList(_context.Users, "Userid", "Userid");
-            return View();
-        }
-
-        // POST: Secretaries/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("SecretaryId,Name,Surname,Department,Userid")] Secretary secretary)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(secretary);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["Userid"] = new SelectList(_context.Users, "Userid", "Userid", secretary.Userid);
-            return View(secretary);
-        }
-
-        // GET: Secretaries/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Secretaries == null)
-            {
-                return NotFound();
-            }
-
-            var secretary = await _context.Secretaries.FindAsync(id);
-            if (secretary == null)
-            {
-                return NotFound();
-            }
-            ViewData["Userid"] = new SelectList(_context.Users, "Userid", "Userid", secretary.Userid);
-            return View(secretary);
-        }
-
-        // POST: Secretaries/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("SecretaryId,Name,Surname,Department,Userid")] Secretary secretary)
-        {
-            if (id != secretary.SecretaryId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(secretary);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!SecretaryExists(secretary.SecretaryId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["Userid"] = new SelectList(_context.Users, "Userid", "Userid", secretary.Userid);
-            return View(secretary);
-        }
-
-        // GET: Secretaries/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Secretaries == null)
-            {
-                return NotFound();
-            }
-
-            var secretary = await _context.Secretaries
-                .Include(s => s.User)
-                .FirstOrDefaultAsync(m => m.SecretaryId == id);
-            if (secretary == null)
-            {
-                return NotFound();
-            }
-
-            return View(secretary);
-        }
-
-        // POST: Secretaries/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Secretaries == null)
-            {
-                return Problem("Entity set 'UniversityDBContext.Secretaries'  is null.");
-            }
-            var secretary = await _context.Secretaries.FindAsync(id);
-            if (secretary != null)
-            {
-                _context.Secretaries.Remove(secretary);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool SecretaryExists(int id)
